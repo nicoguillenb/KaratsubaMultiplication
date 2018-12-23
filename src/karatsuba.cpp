626a8d602bed7d629a075ecca4667ff05867fa3b
Nicolas Guillen <nicguillenb@gmail.com>
2018-12-22 21:57:23 -0300

    UPDATE

--- src/karatsuba.cpp
+++ src/karatsuba.cpp
@@ -3,9 +3,6 @@
 #include "console.h"
 #include "strlib.h"
 #include "math.h"
-#include "gwindow.h" // for GWindow
-#include "simpio.h"  // for getLine
-#include "vector.h"  // for Vector
 using namespace std;
 
 
@@ -14,20 +11,24 @@ string multiply(string x, string y);
 string addition(string x, string y);
 string substraction(string x, string y);
 void addZeros(string &x, string &y);
-void orderNumbers(string &x, string &y);
+bool isPowerOfTwo (int n);
 
 int main() {
     cout << "This program uses the Karatsuba Algorithm to multiply long integers." << endl;
-//    string x = "3141592653589793238462643383279502884197169399375105820974944592";
-//    string y = "2718281828459045235360287471352662497757247093699959574966967627";
-    string x = "92";
-    string y = "27";
+    string x = "3141592653589793238462643383279502884197169399375105820974944592";
+    string y = "2718281828459045235360287471352662497757247093699959574966967627";
+    string testResult = "8539734222673567065463550869546574495034888535765114961879601127067743044893204848617875072216249073013374895871952806582723184";
     cout << "We're going to multiply the following numbers:" << endl;
     cout << "x: " + x << endl;
     cout << "y: " + y << endl;
     string result = multiply(x,y);
     cout << endl;
-    cout << "The Result is: " + result << endl;
+    cout << "The result is: " + result << endl;
+    if (result == testResult){
+        cout << "The result is correct" << endl;
+    } else {
+        cout << "The result is incorrect" << endl;
+    }
     return 0;
 }
 
@@ -39,6 +40,12 @@ string multiply(string x, string y){
         int result = (stringToInteger(x) * stringToInteger(y));
         return integerToString(result);
     } else {
+        if (!isPowerOfTwo(x.length())){
+            while (!isPowerOfTwo(x.length())){
+                x = "0" + x;
+                y = "0" + y;
+            }
+        }
         int n = x.length();
         string a = x.substr(0, n/2);
         string b = x.substr(n/2);
@@ -48,7 +55,8 @@ string multiply(string x, string y){
         string bd = multiply(d,b);
         string pq = multiply(addition(a,b), addition(c,d));
         string abdc = substraction(pq,addition(ac,bd));
-        return addition(ac + zero(n), addition(abdc+ zero(n/2), bd));
+        string result = addition(ac + zero(n), addition(abdc+ zero(n/2), bd));
+        return result;
     }
 }
 
@@ -71,10 +79,10 @@ string addition(string x, string y){
     int xlength = x.length();
     for (int i = 0; i < xlength; i++){
         int length = x.length();
-        if (stringIsInteger(x)) tempX = charToInteger(x[length-1]);
-        x = x.substr(0,length-1);
-        if (stringIsInteger(y)) tempY = charToInteger(y[length-1]);
-        y = y.substr(0,length-1);
+            tempX = charToInteger(x[length-1]);
+            x = x.substr(0,length-1);
+            tempY = charToInteger(y[length-1]);
+            y = y.substr(0,length-1);
         tempResult = integerToString(tempX + tempY + reminder);
         result = tempResult[tempResult.length()-1] + result;
         if (tempResult.length() > 1){
@@ -84,6 +92,8 @@ string addition(string x, string y){
         }
         if (stringIsInteger(tempResult)){
             reminder = stringToInteger(tempResult);
+        } else{
+            reminder = 0;
         }
     }
     if (stringIsInteger(tempResult)){
@@ -97,30 +107,29 @@ string substraction(string x, string y){
     string result;
     string tempResult;
     addZeros(x,y);
-    orderNumbers(x,y);
     int xlength = x.length();
-    bool substraction = false;
+    bool substr = false;
     int helper = 0;
     int tempX;
     int tempY;
     for (int i = 0; i < xlength; i++){
-        if (substraction) {
+        if (substr) {
             helper = -1;
          } else {
             helper = 0;
          }
         int length = x.length();
-        if (stringIsInteger(x)) tempX = charToInteger(x[length-1]);
+        tempX = charToInteger(x[length-1]);
         x = x.substr(0,length-1);
-        if (stringIsInteger(y)) tempY = charToInteger(y[length-1]);
+        tempY = charToInteger(y[length-1]);
         y = y.substr(0,length-1);
         tempX += helper;
-        if (tempX < tempY && stringIsInteger(x)){
+        if (tempX < tempY){
             tempResult = integerToString((tempX + 10) - tempY);
-            substraction = true;
+            substr = true;
         } else {
             tempResult = integerToString(tempX - tempY);
-            substraction = false;
+            substr = false;
         }
         result = tempResult[tempResult.length()-1] + result;
         tempResult = "";
@@ -140,17 +149,15 @@ void addZeros(string &x, string &y){
     }
 }
 
-void orderNumbers(string &x, string &y){
-    int counter = x.length();
-    for (int i = 0; i < counter; i++){
-        if(charToInteger(x[i]) != charToInteger(y[i])){
-            if(charToInteger(x[i]) < charToInteger(y[i])){
-                   string temp = x;
-                   x = y;
-                   y = temp;
-            }
-            return;
-        }
+bool isPowerOfTwo (int n){
+    if (n <= 0){
+        return false;
     }
+    while (n > 1){
+        if (n % 2 != 0){
+            return false;
+        }
+        n = n/2;
+    }
+    return true;
 }
-
